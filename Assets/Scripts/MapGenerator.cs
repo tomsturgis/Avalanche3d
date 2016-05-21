dd1e7fe12679046656c6a4e2af3e701e92f8c241
abe <mrbiglseworth@gmail.com>
2016-05-21 13:56:11 -0400

    added tiles

---
+++ Assets/Scripts/MapGenerator.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MapGenerator : MonoBehaviour {
+
+    public int width;
+    public int height;
+    public GameObject[] backgroundTiles;
+
+    private Transform boardHolder;
+    int[,] map;
+
+    public int[,] GenerateMap()
+    {
+
+        map = new int[width, height];
+
+        return map;
+    }
+
+    void LayoutTiles()
+    {
+        boardHolder = new GameObject("Board").transform;
+
+        for (int x = 0; x < map.GetLength(0); x++)
+        {
+            for (int y = 0; y < map.GetLength(1); y++)
+            {
+                GameObject toInstantiate = backgroundTiles[Random.Range(0, backgroundTiles.Length)];
+                GameObject instance = Instantiate(toInstantiate, new Vector3(x, y, 0f), Quaternion.identity) as GameObject;
+                instance.transform.SetParent(boardHolder);
+            }
+        }
+    }
+
+	// Use this for initialization
+	void Start () {
+        map = GenerateMap();
+        LayoutTiles();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
