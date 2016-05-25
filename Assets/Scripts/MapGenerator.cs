8781946444e69d6b8b859b68c78fb3519cac83c4
abe <mrbiglseworth@gmail.com>
2016-05-24 23:23:28 -0400

    added background sprite, changed code for generating map and adding icicles

--- Assets/Scripts/MapGenerator.cs
+++ Assets/Scripts/MapGenerator.cs
@@ -6,8 +6,9 @@ public class MapGenerator : MonoBehaviour {
     public int width;
     public int height;
     public GameObject[] backgroundTiles;
-
+    public GameObject[] floorTiles;
     private Transform boardHolder;
+    private GameObject toInstantiate;
     int[,] map;
 
     public int[,] GenerateMap()
@@ -26,7 +27,14 @@ public class MapGenerator : MonoBehaviour {
         {
             for (int y = 0; y < map.GetLength(1); y++)
             {
-                GameObject toInstantiate = backgroundTiles[Random.Range(0, backgroundTiles.Length)];
+                if (y==0)
+                {
+                    toInstantiate = floorTiles[Random.Range(0, floorTiles.Length)];
+                }
+                else
+                {
+                    toInstantiate = backgroundTiles[Random.Range(0, backgroundTiles.Length)];
+                }
                 GameObject instance = Instantiate(toInstantiate, new Vector3(x, y, 0f), Quaternion.identity) as GameObject;
                 instance.transform.SetParent(boardHolder);
             }
